a29b80563bfb9dc9c4f75d30d7533b7b40a88961
josieelordi <josieelordi@gmail.com>
2019-04-09 15:36:50 -0400

    it must be looping somewhere

--- classifier.py
+++ classifier.py
@@ -3,8 +3,17 @@
 import os
 import re
 import sys
-
+import preprocess.py
+import spacy
+import string
+import pandas as pd
 
 if __name__ == '__main__':
+	print("BIOOOOOOTCH")
 
-	
+	in_file = sys.argv[1]
+	print(in_file)
+
+	df = load_data(in_file)
+
+	print(df)
