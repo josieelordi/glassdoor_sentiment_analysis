2faaf99a166b8bb484ea848b6f2a185623aabca5
tylerseastman <eastman@umich.edu>
2019-04-11 18:36:16 -0400

    added v little thingz : )

--- classifier.py
+++ classifier.py
@@ -4,13 +4,12 @@
 import os
 import re
 import sys
+import pandas as pd
 from preprocess import load_data, split_data, preprocess
 
 def main(in_file):
     df = load_data(in_file)
-    features, labels = split_data(df)
-    preprocess(features)
-
-
+    features, Y_train, Y_test = split_data(df)
+    X_train, X_test = preprocess(features)
 if __name__ == '__main__':
 	main(sys.argv[1])
